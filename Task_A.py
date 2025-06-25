a19afec67c7455408ec3b2576d7b52c8523778d7
ambs <22094775@imail.sunway.edu.my>
2025-06-25 12:31:35 +0800

    updated Task_A.py

--- Task_A.py
+++ Task_A.py
@@ -6,4 +6,15 @@ Created on Wed Jun 25 12:13:57 2025
 @author: amberlyteoh
 """
 
-task
+import cv2
+import numpy as np
+import os
+
+# === File Paths ===
+video_path = 'videos/street.mp4'
+talking_path = 'videos/talking.mp4'
+endscreen_path = 'videos/endscreen.mp4'
+watermark1_path = 'watermarks/watermark1.png'
+watermark2_path = 'watermarks/watermark2.png'
+processed_path = 'outputs/final_processed_video.avi'
+final_output_path = 'outputs/final_with_endscreen.avi'
